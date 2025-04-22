f42e8617e1ba32d149f9e02eb0b89745a073f9ae
Lucas091285 <lucasl107@nycstudents.net>
2025-04-22 09:23:03 -0400

    Create teacher_mode.py

---
+++ teacher_mode.py
@@ -0,0 +1,23 @@
+Mode = input("Teacher or Student?")
+Teacher = Mode
+Student = Mode
+
+def Key = ('Question'):
+def Value = ('Answer'):
+import json
+
+class Teacher:
+    def __init__(self, Key, Value):
+        self.Key = Key
+        self.Value = Value
+
+    def display_info(self):
+        return f"{self.Key} {self.Vaue}"
+
+    def to_dict(self):
+        return {"Key": self.Key, "value": self. Value}
+
+FlashCards.json = 
+
+while Student == mode2:
+    FlashCards.json
