db3bbb7e66ed4ad7c1c04a0f0393362d2c9e839b
Lucas091285 <lucasl107@nycstudents.net>
2025-04-24 09:25:14 -0400

    Update teacher_mode.py

--- teacher_mode.py
+++ teacher_mode.py
@@ -1,24 +1,31 @@
-Mode = input("Teacher or Student?")
-Teacher = Mode
-Student = Mode
+Teacher = mode_1
+Student = mode_2
 
-def Key = ('Question'):
-def Value = ('Answer'):
-import json
+while Teacher == mode_1:
+    class Teacher:
+        def __init__(self, Key, Value):
+            self.Key = Question
+            self.Value = Answer
 
-class Teacher:
-    def __init__(self, Key, Value):
-        self.Key = Key
-        self.Value = Value
+        def display_info(self):
+            return f"{self.Key} {self.Vaue}"
 
-    def display_info(self):
-        return f"{self.Key} {self.Vaue}"
+        def to_dict(self):
+            return {"Key": self.Key, "value": self. Value}
 
-    def to_dict(self):
-        return {"Key": self.Key, "value": self. Value}
+x = input("What is the question?")
+y = input("What is the answer?")
+if x == exit:
+    break
+print(x, y)
 
-FlashCards.json = 
-
-while Student == mode2:
-    FlashCards.json
-    22
+while Student == mode_2:
+    streak = 0
+    score = 0
+    if y == input:
+        score += 1
+        if y == input:
+            streak += 1
+                else:
+                print(streak is back to 0)
+                        streak = 0
